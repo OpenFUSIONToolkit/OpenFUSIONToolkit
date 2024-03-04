OpenFUSIONToolkit/OpenFUSIONToolkit

commit 6b3439bd35a45ac7ae6b48e5168f01270273c9ef

TokaMaker: Add support for grouping coils into coil sets (#38)
* Add X-point candidate filtering with characteristic scale length
* Add support for arbitrary number of coil regularization terms
* Add HBT example and update ITER and fixed-boundary examples
* Update examples and format notes in Jupyter notebook examples

src/physics/grad_shaf_td.F90
@@ -159,9 +159,12 @@ DO i=1,tMaker_td_obj%gs_eq%ncond_regs
 END DO
 ALLOCATE(tMaker_td_obj%curr_reg(smesh%nreg))
 tMaker_td_obj%curr_reg=0.d0
-DO i=1,tMaker_td_obj%gs_eq%ncoil_regs
-  j=tMaker_td_obj%gs_eq%coil_regions(i)%id
-  tMaker_td_obj%curr_reg(j)=tMaker_td_obj%gs_eq%coil_regions(i)%curr
+DO i=1,tMaker_td_obj%gs_eq%ncoils
+    DO k=1,tMaker_td_obj%gs_eq%ncoil_regs
+        j=tMaker_td_obj%gs_eq%coil_regions(k)%id
+        tMaker_td_obj%curr_reg(j)=tMaker_td_obj%curr_reg(j) &
+        + tMaker_td_obj%gs_eq%coil_currs(i)*tMaker_td_obj%gs_eq%coil_nturns(j,i)
+    END DO
 END DO
 ! Advance using MF-NK method
 CALL build_vac_op(tMaker_td_obj,tMaker_td_obj%vac_op)
@@ -313,9 +316,12 @@ DO i=1,tMaker_td_obj%gs_eq%ncond_regs
 END DO
 ALLOCATE(tMaker_td_obj%curr_reg(smesh%nreg))
 tMaker_td_obj%curr_reg=0.d0
-DO i=1,tMaker_td_obj%gs_eq%ncoil_regs
-    j=tMaker_td_obj%gs_eq%coil_regions(i)%id
-    tMaker_td_obj%curr_reg(j)=tMaker_td_obj%gs_eq%coil_regions(i)%curr
+DO i=1,tMaker_td_obj%gs_eq%ncoils
+    DO k=1,tMaker_td_obj%gs_eq%ncoil_regs
+        j=tMaker_td_obj%gs_eq%coil_regions(k)%id
+        tMaker_td_obj%curr_reg(j)=tMaker_td_obj%curr_reg(j) &
+          + tMaker_td_obj%gs_eq%coil_currs(i)*tMaker_td_obj%gs_eq%coil_nturns(j,i)
+    END DO
 END DO
 ! Advance using MF-NK method
 CALL build_vac_op(tMaker_td_obj,tMaker_td_obj%vac_op)
@@ -364,9 +370,12 @@ IF(dt/=tMaker_td_obj%dt)THEN
     CALL adv_pre%update(.TRUE.)
 END IF
 ! Update coil currents (end of time step)
-DO i=1,tMaker_td_obj%gs_eq%ncoil_regs
-    j=tMaker_td_obj%gs_eq%coil_regions(i)%id
-    tMaker_td_obj%curr_reg(j)=tMaker_td_obj%gs_eq%coil_regions(i)%curr
+DO i=1,tMaker_td_obj%gs_eq%ncoils
+    DO k=1,tMaker_td_obj%gs_eq%ncoil_regs
+        j=tMaker_td_obj%gs_eq%coil_regions(k)%id
+        tMaker_td_obj%curr_reg(j)=tMaker_td_obj%curr_reg(j) &
+        + tMaker_td_obj%gs_eq%coil_currs(i)*tMaker_td_obj%gs_eq%coil_nturns(j,i)
+    END DO
 END DO
 ! Point to profiles in case they changed
 tMaker_td_obj%F=>tMaker_td_obj%gs_eq%I

src/bin/tokamaker_gs.F90
@@ -162,9 +162,9 @@ IF(file_exists)THEN
   CLOSE(io_unit)
   !---Read coil constraint matrix
   IF(.NOT.ASSOCIATED(mygs%coil_reg_mat))THEN
-    ALLOCATE(mygs%coil_reg_mat(mygs%ncoil_regs+1,mygs%ncoil_regs+1))
+    ALLOCATE(mygs%coil_reg_mat(mygs%ncoils+1,mygs%ncoils+1))
     OPEN(NEWUNIT=io_unit,FILE='coil_reg_mat.dat')
-    DO i=1,mygs%ncoil_regs+1
+    DO i=1,mygs%ncoils+1
       READ(io_unit,*)mygs%coil_reg_mat(i,:)
     END DO
     CLOSE(io_unit)

src/bin/tokamaker_wall.F90
@@ -830,8 +830,8 @@ mygs%compute_chi=.FALSE.
 IF(TRIM(coil_file)/='none')THEN
   mygs%coil_file=coil_file
   CALL mygs%load_coils(ignore_inmesh=.TRUE.)
-  DO i=1,mygs%ncoils
-    mygs%coils(i)%curr=0.d0 ! Zero all external coils
+  DO i=1,mygs%ncoils_ext
+    mygs%coils_ext(i)%curr=0.d0 ! Zero all external coils
   END DO
   DO i=1,mygs%ncoil_regs
     mygs%coil_regions(i)%curr=0.d0 ! Zero all internal coil regions
@@ -955,20 +955,20 @@ DO i=1,self%ncond_regs
           CASE(1)
             IF(grid_3d)THEN
               CALL nonax_indcouple(self%cond_regions(i)%nc_quad,rc,self%cond_regions(i)%extent, &
-              eigs,self%coils(k)%pt,self%coils(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall, &
+              eigs,self%coils_ext(k)%pt,self%coils_ext(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall, &
               rz_grid,rz_correction)
             ELSE 
               CALL nonax_indcouple(self%cond_regions(i)%nc_quad,rc,self%cond_regions(i)%extent, &
-              eigs,self%coils(k)%pt,self%coils(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall)
+              eigs,self%coils_ext(k)%pt,self%coils_ext(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall)
             END IF
           CASE(2)
             IF(grid_3d)THEN
               CALL nonax_rescouple(self%cond_regions(i)%nc_quad,rc,self%cond_regions(i)%extent, &
-              eigs,self%coils(k)%pt,self%coils(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall, &
+              eigs,self%coils_ext(k)%pt,self%coils_ext(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall, &
               eta,rz_grid,rz_correction)
             ELSE 
               CALL nonax_rescouple(self%cond_regions(i)%nc_quad,rc,self%cond_regions(i)%extent, &
-              eigs,self%coils(k)%pt,self%coils(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall,eta)
+              eigs,self%coils_ext(k)%pt,self%coils_ext(k)%ncoils,correction(:,j),sensors,nsensor,mirror_wall,eta)
             END IF
           CASE(3)
             IF(grid_3d)THEN

src/examples/TokaMaker/CMakeLists.txt
@@ -3,6 +3,11 @@ install(FILES fixed_boundary/fixed_boundary_ex1.ipynb DESTINATION examples/TokaM
 install(FILES fixed_boundary/fixed_boundary_ex2.ipynb DESTINATION examples/TokaMaker/fixed_boundary COMPONENT examples)
 install(FILES fixed_boundary/gNT_example DESTINATION examples/TokaMaker/fixed_boundary COMPONENT examples)
 #
+install(FILES HBT/HBT_geom.json DESTINATION examples/TokaMaker/HBT COMPONENT examples)
+install(FILES HBT/HBT_mesh_ex.ipynb DESTINATION examples/TokaMaker/HBT COMPONENT examples)
+install(FILES HBT/HBT_eq_ex.ipynb DESTINATION examples/TokaMaker/HBT COMPONENT examples)
+install(FILES HBT/HBT_mesh.h5 DESTINATION examples/TokaMaker/HBT COMPONENT examples)
+#
 install(FILES ITER/ITER_mesh_ex.ipynb DESTINATION examples/TokaMaker/ITER COMPONENT examples)
 install(FILES ITER/ITER_baseline_ex.ipynb DESTINATION examples/TokaMaker/ITER COMPONENT examples)
 install(FILES ITER/ITER_geom.json DESTINATION examples/TokaMaker/ITER COMPONENT examples)

commit 56e47e376a0de19f1727f09d6da1c3314b2bfa71

TokaMaker: Fix bug in time-dependent solves introduced in 6b3439b

src/physics/grad_shaf_td.F90
@@ -370,11 +370,12 @@ IF(dt/=tMaker_td_obj%dt)THEN
     CALL adv_pre%update(.TRUE.)
 END IF
 ! Update coil currents (end of time step)
+tMaker_td_obj%curr_reg=0.d0
 DO i=1,tMaker_td_obj%gs_eq%ncoils
     DO k=1,tMaker_td_obj%gs_eq%ncoil_regs
         j=tMaker_td_obj%gs_eq%coil_regions(k)%id
         tMaker_td_obj%curr_reg(j)=tMaker_td_obj%curr_reg(j) &
-        + tMaker_td_obj%gs_eq%coil_currs(i)*tMaker_td_obj%gs_eq%coil_nturns(j,i)
+            + tMaker_td_obj%gs_eq%coil_currs(i)*tMaker_td_obj%gs_eq%coil_nturns(j,i)
     END DO
 END DO
 ! Point to profiles in case they changed