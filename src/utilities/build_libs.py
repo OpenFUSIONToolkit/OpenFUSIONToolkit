8cf4dc5808229c8352aa7125fddd5a58493d2786
Chris Hansen <hansec@uw.edu>
2024-03-05 22:29:05 -0500

    Additional updates

--- src/utilities/build_libs.py
+++ src/utilities/build_libs.py
@@ -430,7 +430,7 @@ def build_cmake_script(mydict,build_debug=False,use_openmp=False,build_python=Fa
             cmake_lines.append("-DOFT_UMFPACK_ROOT:PATH={0}".format(mydict["UMFPACK_ROOT"]))
     if tmp_dict['OS_TYPE'] == 'Darwin':
         result, errcode = run_command("sw_vers --productVersion")
-        if (errcode == 0) and (result.split('.')[0] == '13'):
+        if (errcode == 0) and ((result.split('.')[0] == '13') or (result.split('.')[0] == '14' and int(result.split('.')[1]) < 3)):
             cmake_lines.append('-DCMAKE_EXE_LINKER_FLAGS:STRING="-Wl,-ld_classic"')
             cmake_lines.append('-DCMAKE_SHARED_LINKER_FLAGS:STRING="-Wl,-ld_classic"')
     cmake_lines += [os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))]
