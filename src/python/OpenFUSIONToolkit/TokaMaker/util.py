OpenFUSIONToolkit/OpenFUSIONToolkit

commit 60e1ecedf1313d29bdc8010e7f1c1068c167f50f

ThinCurr: Fix bugs in B-field operator construction and hole location (#179)
 - Improve output on various errors in python interface

src/python/OpenFUSIONToolkit/TokaMaker/util.py
@@ -60,11 +60,7 @@ def create_spline_flux_fun(npts,x,y,axis_bc=[1,0.0],edge_bc=[1,0.0],normalize=Tr
     @param edge_bc [SciPy BC specification](https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.CubicSpline.html) on LCFS (\f$ \hat{\psi} = 1 \f$)
     @result Flux function definition dictionary
     '''
-    try:
-        from scipy.interpolate import CubicSpline
-    except ImportError:
-        print("Spline flux function requires SciPy")
-        raise
+    from scipy.interpolate import CubicSpline
     prof = CubicSpline(x,y,bc_type=[axis_bc,edge_bc])
     x_sample = numpy.linspace(0.0,1.0,npts)
     prof = {

src/python/OpenFUSIONToolkit/util.py
@@ -11,6 +11,7 @@
 '''
 import os
 import subprocess
+import re
 import numpy
 import h5py
 from collections import OrderedDict
@@ -21,6 +22,17 @@ from ._interface import oftpy_dump_cov
 mu0 = numpy.pi*4.E-7
 ## Electron charge
 eC = 1.60217663e-19
+
+
+def oft_warning(warning,details=[]):
+    '''! Print warning in common format
+
+    @param warning String describing warning
+    @param details List of additional details
+    '''
+    print("Warning: {0}".format(warning))
+    for detail_line in details:
+        print("         {0}".format(detail_line))
 
 
 def run_shell_command(command, timeout=10, env_vars={}):
@@ -42,7 +54,7 @@ def run_shell_command(command, timeout=10, env_vars={}):
     except subprocess.TimeoutExpired:
         pid.kill()
         outs, _ = pid.communicate()
-        print("WARNING: Command timeout")
+        oft_warning("Timeout for command `{0}`".format(command))
     errcode = pid.poll()
     result = outs.decode("utf-8")
     return result, errcode
@@ -110,7 +122,7 @@ def read_fortran_namelist(file0, silent=True, b_arr=False):
 			if equal_loc >= 0.0:
 				equal_idx.append(i)
 
-		f_lines[brk_idx[np.argmin(abs(brk_idx-np.max(equal_idx)))]] = 'b_arr = '
+		f_lines[brk_idx[numpy.argmin(abs(brk_idx-numpy.max(equal_idx)))]] = 'b_arr = '
 
 	#Prune everything after the comments and remove line breaks 
 	end_idx = len(f_lines)
@@ -161,7 +173,7 @@ def read_fortran_namelist(file0, silent=True, b_arr=False):
 				mult_block = ' '.join(datalines[block_idx[i]:block_idx[i+1]])
 				equal_loc = mult_block.find('=')
 				mult_block = mult_block[equal_loc+1:].strip()
-				#data_dict[key_list[i]] = np.array(mult_block.split(' '))#.astype('float')
+				#data_dict[key_list[i]] = numpy.array(mult_block.split(' '))#.astype('float')
 				dupl_loc = mult_block.find('*')
 				if dupl_loc >= 0:
 					mult_block = mult_block.split()
@@ -178,14 +190,14 @@ def read_fortran_namelist(file0, silent=True, b_arr=False):
 					mult_block = ' '.join(mult_block)
 
 					try:
-						data_dict[key_list[i]] = np.array(mult_block.split()).astype('float')
+						data_dict[key_list[i]] = numpy.array(mult_block.split()).astype('float')
 
 					except:
 						data_dict[key_list[i]] = mult_block
 				else:
 					try:
 						if len(mult_block.split()) > 1:  #If longer than one entry, make it an array
-							data_dict[key_list[i]] = np.array(mult_block.split()).astype('float')
+							data_dict[key_list[i]] = numpy.array(mult_block.split()).astype('float')
 						else:    #if it is one entry, make it a float or an integer
 							data_dict[key_list[i]] = eval(mult_block.split()[0])
 
@@ -212,22 +224,22 @@ def read_fortran_namelist(file0, silent=True, b_arr=False):
 
 					mult_block = ' '.join(mult_block) 
 					try:
-						data_dict[key_list[i]] = np.array(mult_block.split()).astype('float')
+						data_dict[key_list[i]] = numpy.array(mult_block.split()).astype('float')
 					except:
 						data_dict[key_list[i]] = mult_block
 				else:
 					try:
-						data_dict[key_list[i]] = np.array(mult_block.split()).astype('float')
+						data_dict[key_list[i]] = numpy.array(mult_block.split()).astype('float')
 					except:
 						data_dict[key_list[i]] = mult_block
 
 		elif key_list[i] == 'b_arr':
 			b_arr_len = len(datalines[block_idx[i]+1:len(datalines)])
-			data_dict['b_arr'] = np.zeros((b_arr_len,6))
+			data_dict['b_arr'] = numpy.zeros((b_arr_len,6))
 
 			for j in range(b_arr_len):
 				try:
-					b_line = np.array(re.split(r'(\s+)', datalines[block_idx[i]+1+j]))
+					b_line = numpy.array(re.split(r'(\s+)', datalines[block_idx[i]+1+j]))
 					b_line_space_idx = []
 					b_line_rmv_idx = []
 
@@ -243,14 +255,14 @@ def read_fortran_namelist(file0, silent=True, b_arr=False):
 						else:
 							b_line[b_line_space_idx[l]] = 0.0
 
-					b_line = np.delete(b_line, b_line_rmv_idx)
+					b_line = numpy.delete(b_line, b_line_rmv_idx)
 
-					#b_line = np.array(datalines[block_idx[i]+1+j].split()).astype('float')
+					#b_line = numpy.array(datalines[block_idx[i]+1+j].split()).astype('float')
 					data_dict['b_arr'][j][0:len(b_line)] = b_line 
 
 
 				except:
-					data_dict['b_arr'][j][:] = np.nan
+					data_dict['b_arr'][j][:] = numpy.nan
 
 			if 'RF' not in key_list:
 				try:

src/physics/thin_wall.F90
@@ -1950,35 +1950,11 @@ REAL(r8) :: evec_i(3,3),evec_j(3),pts_i(3,3),pt_i(3),pt_j(3),diffvec(3),ecc(3)
 REAL(r8) :: r1,z1,rmag,cvec(3),cpt(3),tmp,area_i,dl_min,dl_max,norm_j(3),f(3),pot_tmp,pot_last
 REAL(r8), ALLOCATABLE :: atmp(:,:,:)
 REAL(8), PARAMETER :: B_dx = 1.d-6
-INTEGER(4) :: i,ii,j,jj,ik,jk,k,kk,iquad,hash_tmp(4),file_counts(4)
-LOGICAL :: is_neighbor,exists
+INTEGER(4) :: i,ii,j,jj,ik,jk,k,kk,iquad
+LOGICAL :: is_neighbor
 CLASS(oft_bmesh), POINTER :: bmesh
 TYPE(oft_quad_type), ALLOCATABLE :: quads(:)
-IF(TRIM(save_file)/='none')THEN
-  INQUIRE(FILE=TRIM(save_file),EXIST=exists)
-  IF(exists)THEN
-    hash_tmp(1) = self%nelems
-    hash_tmp(2) = self%mesh%nc
-    hash_tmp(3) = oft_simple_hash(C_LOC(self%mesh%lc),INT(4*3*self%mesh%nc,8))
-    hash_tmp(4) = oft_simple_hash(C_LOC(self%mesh%r),INT(8*3*self%mesh%np,8))
-    WRITE(*,*)'  Loading B-field operator from file: ',TRIM(save_file)
-    CALL hdf5_read(file_counts,TRIM(save_file),'MODEL_hash')
-    IF(exists.AND.ALL(file_counts==hash_tmp))THEN
-      ALLOCATE(self%Bel(self%nelems,self%mesh%np,3))
-      CALL hdf5_read(self%Bel(:,:,1),TRIM(save_file),'Bel_X',success=exists)
-      IF(exists)CALL hdf5_read(self%Bel(:,:,2),TRIM(save_file),'Bel_Y',success=exists)
-      IF(exists)CALL hdf5_read(self%Bel(:,:,3),TRIM(save_file),'Bel_Z',success=exists)
-      IF(exists)THEN
-        ALLOCATE(self%Bdr(self%mesh%np,self%n_icoils,3))
-        CALL hdf5_read(self%Bdr(:,:,1),TRIM(save_file),'Bdr_X',success=exists)
-        IF(exists)CALL hdf5_read(self%Bdr(:,:,2),TRIM(save_file),'Bdr_X',success=exists)
-        IF(exists)CALL hdf5_read(self%Bdr(:,:,3),TRIM(save_file),'Bdr_X',success=exists)
-      END IF
-    END IF
-  END IF
-  IF(exists)RETURN
-  DEALLOCATE(self%Bel,self%Bdr)
-END IF
+IF(PRESENT(save_file))CALL load_from_file()
 !
 bmesh=>self%mesh
 ALLOCATE(quads(18))
@@ -2146,6 +2122,42 @@ DO i=1,bmesh%np
 END DO
 self%Bdr=self%Bdr*mu0/(4.d0*pi)
 !
+IF(PRESENT(save_file))CALL load_from_file()
+CONTAINS
+SUBROUTINE load_from_file()
+INTEGER(4) :: hash_tmp(4),file_counts(4)
+LOGICAL :: exists
+IF(TRIM(save_file)/='none')THEN
+  INQUIRE(FILE=TRIM(save_file),EXIST=exists)
+  IF(exists)THEN
+    hash_tmp(1) = self%nelems
+    hash_tmp(2) = self%mesh%nc
+    hash_tmp(3) = oft_simple_hash(C_LOC(self%mesh%lc),INT(4*3*self%mesh%nc,8))
+    hash_tmp(4) = oft_simple_hash(C_LOC(self%mesh%r),INT(8*3*self%mesh%np,8))
+    WRITE(*,*)'  Loading B-field operator from file: ',TRIM(save_file)
+    CALL hdf5_read(file_counts,TRIM(save_file),'MODEL_hash')
+    IF(exists.AND.ALL(file_counts==hash_tmp))THEN
+      ALLOCATE(self%Bel(self%nelems,self%mesh%np,3))
+      CALL hdf5_read(self%Bel(:,:,1),TRIM(save_file),'Bel_X',success=exists)
+      IF(exists)CALL hdf5_read(self%Bel(:,:,2),TRIM(save_file),'Bel_Y',success=exists)
+      IF(exists)CALL hdf5_read(self%Bel(:,:,3),TRIM(save_file),'Bel_Z',success=exists)
+      IF(exists)THEN
+        ALLOCATE(self%Bdr(self%mesh%np,self%n_icoils,3))
+        CALL hdf5_read(self%Bdr(:,:,1),TRIM(save_file),'Bdr_X',success=exists)
+        IF(exists)CALL hdf5_read(self%Bdr(:,:,2),TRIM(save_file),'Bdr_X',success=exists)
+        IF(exists)CALL hdf5_read(self%Bdr(:,:,3),TRIM(save_file),'Bdr_X',success=exists)
+      END IF
+    END IF
+    IF(exists)RETURN
+    IF(ASSOCIATED(self%Bel))DEALLOCATE(self%Bel)
+    IF(ASSOCIATED(self%Bdr))DEALLOCATE(self%Bdr)
+  END IF
+END IF
+END SUBROUTINE load_from_file
+!
+SUBROUTINE save_to_file()
+INTEGER(4) :: hash_tmp(4),file_counts(4)
+LOGICAL :: exists
 IF(TRIM(save_file)/='none')THEN
   hash_tmp(1) = self%nelems
   hash_tmp(2) = self%mesh%nc
@@ -2165,6 +2177,7 @@ IF(TRIM(save_file)/='none')THEN
     END IF
   END IF
 END IF
+END SUBROUTINE save_to_file
 END SUBROUTINE tw_compute_Bops
 !---------------------------------------------------------------------------------
 !> Setup hole definition for ordered chain of vertices
@@ -2348,7 +2361,7 @@ DO i=1,ncoils
   !---Get sensor flag
   IF(xml_hasAttribute(coil_set,"sens_mask"))THEN
     CALL xml_extractDataAttribute(coil_set,"sens_mask",coil_tmp%sens_mask,num=nread,iostat=ierr)
-    IF(coil_tmp%sens_mask)WRITE(*,'(2A,I4,A)')oft_indent,'Masking coil ',i,' from sensors'
+    IF(coil_tmp%sens_mask)WRITE(*,'(2A,I6,A)')oft_indent,'Masking coil ',i,' from sensors'
   END IF
   ALLOCATE(coil_tmp%coils(coil_tmp%ncoils))
   DO j=1,coil_tmp%ncoils
@@ -2357,21 +2370,21 @@ DO i=1,ncoils
     IF(xml_hasAttribute(coil,"path"))THEN
       CALL xml_extractDataAttribute(coil,"path",coil_path,num=nread,iostat=ierr)
       IF(ierr/=0)THEN
-        WRITE(coil_ind,'(I4,2X,I4)')i,j
+        WRITE(coil_ind,'(I6,2X,I6)')i,j
         CALL oft_abort('Error reading "path" in coil '//coil_ind,'tw_load_coils',__FILE__)
       END IF
       ipath=INDEX(coil_path,":")
       IF(ipath==0)THEN
-        WRITE(coil_ind,'(I4,2X,I4)')i,j
+        WRITE(coil_ind,'(I6,2X,I6)')i,j
         CALL oft_abort('Misformatted "path" attribute in coil '//coil_ind,'tw_load_coils',__FILE__)
       END IF
       CALL hdf5_field_get_sizes(coil_path(1:ipath-1),coil_path(ipath+1:OFT_PATH_SLEN),ndims,dim_sizes)
       IF(ndims<0)THEN
-        WRITE(coil_ind,'(I4,2X,I4)')i,j
+        WRITE(coil_ind,'(I6,2X,I6)')i,j
         CALL oft_abort('Failed to read HDF5 data sizes for coil '//coil_ind,'tw_load_coils',__FILE__)
       END IF
       IF(dim_sizes(1)/=3)THEN
-        WRITE(coil_ind,'(I4,2X,I4)')i,j
+        WRITE(coil_ind,'(I6,2X,I6)')i,j
         CALL oft_abort('Incorrect first dimension of HDF5 dataset for coil '//coil_ind,'tw_load_coils',__FILE__)
       END IF
       coil_tmp%coils(j)%npts=dim_sizes(2)
@@ -2379,7 +2392,7 @@ DO i=1,ncoils
       ALLOCATE(coil_tmp%coils(j)%pts(3,coil_tmp%coils(j)%npts))
       CALL hdf5_read(coil_tmp%coils(j)%pts,coil_path(1:ipath-1),coil_path(ipath+1:OFT_PATH_SLEN),success=success)
       IF(.NOT.success)THEN
-        WRITE(coil_ind,'(I4,2X,I4)')i,j
+        WRITE(coil_ind,'(I6,2X,I6)')i,j
         CALL oft_abort('Failed to read HDF5 data for coil '//coil_ind,'tw_load_coils',__FILE__)
       END IF
     ELSE
@@ -2394,7 +2407,7 @@ DO i=1,ncoils
         CASE(1)
           CALL xml_extractDataContent(coil,pts_tmp,num=nread,iostat=ierr)
           IF(ierr/=0)THEN
-            WRITE(coil_ind,'(I4,2X,I4)')i,j
+            WRITE(coil_ind,'(I6,2X,I6)')i,j
             CALL oft_abort('Error reading circular coil '//coil_ind,'tw_load_coils',__FILE__)
           END IF
           IF(coil_tmp%coils(j)%npts==0)coil_tmp%coils(j)%npts=181
@@ -2407,7 +2420,7 @@ DO i=1,ncoils
           ALLOCATE(coil_tmp%coils(j)%pts(3,coil_tmp%coils(j)%npts))
           CALL xml_extractDataContent(coil,coil_tmp%coils(j)%pts,num=nread,iostat=ierr)
           IF(ierr/=0)THEN
-            WRITE(coil_ind,'(I4,2X,I4)')i,j
+            WRITE(coil_ind,'(I6,2X,I6)')i,j
             CALL oft_abort('Error reading coil '//coil_ind,'tw_load_coils',__FILE__)
           END IF
       END SELECT

src/python/OpenFUSIONToolkit/ThinCurr/meshing.py
@@ -11,7 +11,7 @@
 '''
 import numpy
 import h5py
-from ..util import write_native_mesh
+from ..util import write_native_mesh, oft_warning
 
 
 def write_ThinCurr_mesh(filename, r, lc, reg, holes=[], closures=[], pmap=None, nfp=None):
@@ -94,11 +94,7 @@ def build_regcoil_grid(filename, field_suffix, ntheta, nphi, full_torus=False):
     @param full_torus Construct grid for the full torus (default: one field period)
     @result `rgrid` Structed phi-theta grid of points [nphi,ntheta,3], `nfp` Number of field periods
     '''
-    try:
-        import netCDF4
-    except ImportError:
-        print('"netCDF4" required to load REGCOIL files')
-        raise
+    import netCDF4
     with netCDF4.Dataset(filename) as file:
         rmnc = file['rmnc_{0}'.format(field_suffix)][:]
         zmns = file['zmns_{0}'.format(field_suffix)][:],
@@ -191,15 +187,9 @@ def build_torus_bnorm_grid(filename,nsample,nphi,resample_type='theta',use_splin
         for i in range(nsample):
             resample_grid[i]=i*mode_in[npts,3]/nsample
     else:
-        print("Invalid resampling type")
-        return None
-        #raise ValueError("Invalid resampling type")
+       raise ValueError("Invalid resampling type")
     if use_spline:
-        try:
-            from scipy.interpolate import CubicSpline
-        except ImportError:
-            print("SciPy required for spline interpolation")
-            raise
+        from scipy.interpolate import CubicSpline
         splines = [
             CubicSpline(mode_in[:npts+2,3], mode_in[:npts+2,0], axis=0, bc_type='not-a-knot'),
             CubicSpline(mode_in[:npts+2,3], mode_in[:npts+2,1], axis=0, bc_type='not-a-knot'),
@@ -219,7 +209,7 @@ def build_torus_bnorm_grid(filename,nsample,nphi,resample_type='theta',use_splin
     cos_sum = mode_resample[:,5].sum(); cos_sum_abs = max(1.E-14,abs(mode_resample[:,5]).sum())
     print('  Mode pair sums {0:.4E} {1:.4E}'.format(sin_sum,cos_sum))
     if (abs(sin_sum/sin_sum_abs) > 1.E-4) or (abs(cos_sum/cos_sum_abs) > 1.E-4):
-        print('Warning: Large net flux present in one or more modes! This may indicate an error.')
+        oft_warning('Large net flux present in one or more modes! This may indicate an error.')
     #
     phi_grid = numpy.linspace(0.0,2.0*numpy.pi/nmode,nphi,endpoint=(nmode>1))
     r = numpy.zeros((nphi,nsample,3))

src/python/OpenFUSIONToolkit/TokaMaker/meshing.py
@@ -13,6 +13,7 @@ import json
 import math
 import numpy
 from .._interface import *
+from ..util import oft_warning
 
 ## @cond
 class triangle_struct(c_struct):
@@ -835,7 +836,7 @@ class Mesh:
                     ])
                     seg_tmp.append(len(pts_out)-1)
             elif dl[-1] < segment[2]:
-                print("  Warning: small feature (dl={0:.2E}) detected at point {1} ({2}, {3})".format(dl[-1], segment[0][0], *pts_tmp[0,:]))
+                oft_warning("Small feature (dl={0:.2E}) detected at point {1} ({2}, {3})".format(dl[-1], segment[0][0], *pts_tmp[0,:]))
             seg_tmp.append(segment[0][-1])
             self._resampled_segments.append(seg_tmp)
         # Reindex points
@@ -951,7 +952,7 @@ class Region:
                     tang = self._points[i+1,:] - self._points[i,:]
                 tang_norm = numpy.linalg.norm(tang)
                 if tang_norm < self._dx_curve/1.E3:
-                    print("  Warning: repeated points detected at point {0} ({1}, {2})".format(i, *self._points[i,:]))
+                    oft_warning("Repeated points detected at point {0} ({1}, {2})".format(i, *self._points[i,:]))
                     continue
                 tang /= tang_norm
                 if i > 0:
@@ -959,7 +960,7 @@ class Region:
                     if angle < keep_tol:
                         keep_points.append(i)
                         if angle < sliver_tol:
-                            print("  Warning: sliver (angle={0:.1F}) detected at point {1} ({2}, {3})".format(180.-numpy.arccos(angle)*180./numpy.pi, i, *self._points[i,:]))
+                            oft_warning("Sliver (angle={0:.1F}) detected at point {1} ({2}, {3})".format(180.-numpy.arccos(angle)*180./numpy.pi, i, *self._points[i,:]))
                 tangp = tang
             keep_points.append(nv+1)
             # Index segments
@@ -1001,7 +1002,7 @@ class Region:
                             numpy.interp(dl_samp,dl,pts_tmp[:,1])
                         ])
                 elif dl[-1] < self._small_thresh:
-                    print("  Warning: small feature (dl={0:.2E}) detected at point {1} ({2}, {3})".format(dl[-1], segment[0], *pts_tmp[0,:]))
+                    oft_warning("Small feature (dl={0:.2E}) detected at point {1} ({2}, {3})".format(dl[-1], segment[0], *pts_tmp[0,:]))
             self._resampled_points = numpy.asarray(pts_out)
         return self._resampled_points
 

src/python/OpenFUSIONToolkit/_core.py
@@ -14,7 +14,7 @@ import shutil
 import tempfile
 import numpy
 from ._interface import *
-from .util import run_shell_command
+from .util import run_shell_command, oft_warning
 
 
 class OFT_env():
@@ -54,11 +54,11 @@ class OFT_env():
             except:
                 pass
         if self.ncpus is None:
-            print("WARNING: Could not detect number of physical cores.")
+            oft_warning("Could not detect number of physical cores")
         else:
             if nthreads > self.ncpus:
-                print('Warning: Request of {0} threads exceeds {1} physical cores detected by OFT (excluding "efficiency" cores)'.format(nthreads, self.ncpus))
-                print("         If correct, this will significantly degrade performance.")
+                oft_warning('Request of {0} threads exceeds {1} physical cores detected by OFT (excluding "efficiency" cores)'.format(nthreads, self.ncpus),
+                            details=["If correct, this may significantly degrade performance due to oversubcription"])
         ## ID of Python interpreter process
         self.pid = os.getpid()
         if unique_tempfiles == 'global':
@@ -67,21 +67,21 @@ class OFT_env():
             try:
                 os.mkdir(self.tempdir)
             except:
-                print("Could not make temporary directory")
-                raise
+                raise IOError("Could not create temporary directory."
+                              "This usually means you have tried to instantiate more than one instance of `OFT_env` within a single python kernel.")
         elif unique_tempfiles == 'local_dir':
             self.tempdir = os.path.join(os.getcwd(),'oft_tmp-{0}'.format(self.pid))
             try:
                 os.mkdir(self.tempdir)
             except:
-                print("Could not make temporary directory")
-                raise
+                raise IOError("Could not create temporary directory."
+                              "This usually means you have tried to instantiate more than one instance of `OFT_env` within a single python kernel.")
         elif unique_tempfiles == 'local_file':
             self.tempdir = None
         elif unique_tempfiles == 'none':
             self.tempdir = None
             self.pid = None
-            print("Warning: Using non-unique names/locations for temporary files can lead to conflicts if multiple python instances are used in the same directory")
+            oft_warning("Using non-unique names/locations for temporary files can lead to conflicts if multiple python instances are used in the same directory")
         else:
             raise ValueError('Unknown value "{0}" for "unique_tempfiles"'.format(unique_tempfiles))
         ## Number of threads for execution
@@ -193,9 +193,9 @@ class OFT_env():
             try:
                 shutil.rmtree(self.tempdir)
             except:
-                print('Warning: unable to delete temporary directory "{0}"'.format(self.tempdir))
+                oft_warning('Unable to delete temporary directory "{0}"'.format(self.tempdir))
         else:
             try:
                 os.remove(self.oft_ifile)
             except:
-                print('Warning: unable to delete temporary file "{0}"'.format(self.oft_ifile))
+                oft_warning('Unable to delete temporary file "{0}"'.format(self.oft_ifile))

src/python/OpenFUSIONToolkit/io.py
@@ -76,8 +76,7 @@ class histfile:
                     self.field_types.append('d')
                     self.field_sizes.append(8)
                 else:
-                    print('unknown type')
-                    raise IOError
+                    raise IOError('Unknown type "{0}" for history file variable'.format(field))
             #
             self.field_repeats = []
             for field in base_dict['field_sizes'].split():
@@ -107,8 +106,7 @@ class histfile:
                 self.dim = None
             #
             if (self.dim != None) and (self.nfields > 1):
-                print("Arrays not supported with multiple fields")
-                raise ValueError
+                raise ValueError("Arrays with multiple fields not supported")
             #
             self.field_tags = []
             for _ in range(self.nfields):
@@ -142,8 +140,7 @@ class histfile:
                 elif (tmp_type[0] + tmp[0]) == 'r8':
                     self.field_types.append('d')
                 else:
-                    print('unknown type')
-                    raise IOError
+                    raise IOError('Unknown type "{0}" for history file variable'.format(tmp_type[0] + tmp[0]))
             self.offset += 4
             #
             if self.nfields == 1:
@@ -192,12 +189,10 @@ class histfile:
             self.nlines += 1
             if stream_data:
                 if (head_val[0] != self.line_length) or (tail_val[0] != self.nfields):
-                    print('Bad data line ({0})'.format(self.nlines))
-                    raise IOError
+                    raise IOError('Bad data line ({0})'.format(self.nlines))
             else:
                 if (head_val[0] != self.line_length) or (tail_val[0] != self.line_length):
-                    print('Bad data line ({0})'.format(self.nlines))
-                    raise IOError
+                    raise IOError('Bad data line ({0})'.format(self.nlines))
             if self.nfields > 1:
                 k = 0
                 for (j, field) in enumerate(self.field_tags):
@@ -218,28 +213,18 @@ class histfile:
 
     def save_to_matlab(self,filename):
         r'''! Convert data to MATLAB format'''
-        try:
-            import scipy.io as sio
-        except:
-            print("Unable to load SCIPY")
-            raise
-        else:
-            print("Converting file to MATLAB format")
-            sio.savemat(filename, self._data, oned_as='row')
+        import scipy.io as sio
+        print("Converting file to MATLAB format")
+        sio.savemat(filename, self._data, oned_as='row')
 
     def save_to_hdf5(self,filename):
         r'''! Convert data to HDF5 format'''
-        try:
-            import h5py
-        except:
-            print("Unable to load H5PY")
-            raise
-        else:
-            print("Converting file to HDF5 format")
-            with h5py.File(filename, 'w') as fid:
-                for (ind, field) in enumerate(self.field_tags):
-                    fid[field] = self._data[field]
-                    fid[field].attrs['description'] = self.field_descriptions[field]
+        import h5py
+        print("Converting file to HDF5 format")
+        with h5py.File(filename, 'w') as fid:
+            for (ind, field) in enumerate(self.field_tags):
+                fid[field] = self._data[field]
+                fid[field].attrs['description'] = self.field_descriptions[field]
 
     def get(self, keyname, value=None):
         return self._data.get(keyname,value)
@@ -346,11 +331,7 @@ class XDMF_plot_mesh:
 
         @returns `pyvista.UnstructuredGrid` object for grid
         '''
-        try:
-            import pyvista
-        except ImportError:
-            print('Failed to load "pyvista" package')
-            raise
+        import pyvista
         if self.type == 31:
             celltype = pyvista.CellType.TETRA
             ncv = 4

src/utilities/scripts/ThinCurr_compute_holes.py
@@ -318,8 +318,11 @@ class trimesh:
                     next_pt = cycle_tmp[i]
                     for k in range(self.kpf[next_pt],self.kpf[next_pt+1]):
                         face = self.lpf[k]
-                        if self.bp[self.lf[face,0]] and self.bp[self.lf[face,1]] and self.bp[self.lf[face,2]]:
-                            break
+                        for l in range(3):
+                            if not (self.bp[self.lf[face,l]] and (self.lf[face,l] in cycle_tmp)):
+                                break
+                        else:
+                            break # Point lies on an interior corner of the cycle (skip)
                     else:
                         break
                 else:

commit bab38e7cc034a8e7308b965b090dc1c6846efe1d

TokaMaker: Fix bugs in EFIT readers introduced in 60e1ece (#183)

src/python/OpenFUSIONToolkit/TokaMaker/util.py
@@ -284,14 +284,8 @@ def read_mhdin(path, e_coil_names=None, f_coil_names=None):
         machine_dict[key] = names        
         
     e_coil_dict = OrderedDict()
-    raw['ECID'] = [x for x in raw['ECID'] if len(x.strip()) > 0]
-    e_coil_vars = ['RE', 'ZE', 'WE', 'HE']
-    for var in e_coil_vars:
-        raw[var] = raw[var].split()
 
     for i in range(len(raw['ECID'])):
-        if raw['ECID'][i].strip() == '':
-            continue
         idx = int(raw['ECID'][i]) - 1
         e_coil_name = "ECOIL{:03d}".format(idx + 1)
         if e_coil_names:
@@ -301,13 +295,7 @@ def read_mhdin(path, e_coil_names=None, f_coil_names=None):
         e_coil_dict[e_coil_name].append([float(raw['RE'][i]), float(raw['ZE'][i]), float(raw['WE'][i]), float(raw['HE'][i])])
     machine_dict['ECOIL'] = e_coil_dict
 
-
-    f_coil_vars = ['RF', 'ZF', 'WF', 'HF', 'TURNFC']
-    for var in f_coil_vars:
-        raw[var] = raw[var].split()
-
     f_coil_dict = OrderedDict()
-    raw['FCID']= raw['FCID'].split()
     for i in range(len(raw['FCID'])):
         f_coil_name = "FCOIL{:03d}".format(i + 1)
         if f_coil_names:
@@ -317,7 +305,7 @@ def read_mhdin(path, e_coil_names=None, f_coil_names=None):
 
     probe_angle_dict = OrderedDict()
     i = 0
-    probe_angles = raw['AMP2'].split()
+    probe_angles = raw['AMP2']
     for probe_name in machine_dict['MPNAM2']:
         probe_angle_dict[probe_name] = float(probe_angles[i])
         i = i + 1
@@ -339,51 +327,33 @@ def read_kfile(path, machine_dict, e_coil_names=None, f_coil_names=None):
     @result raw Dictionary containing all other data from k-file.
     '''
     raw = read_fortran_namelist(path)
-
-    def parse_selected(selected_str):
-        tokens = selected_str.split()
-        weights = []
-        for t in tokens:
-            subtokens = t.split('*')
-            if len(subtokens) == 2:
-                n = int(float(subtokens[0]))
-                for _ in range(n):
-                    weights.append(float(subtokens[1]))
-            else:
-                weights.append(float(subtokens[0]))
-        return weights
-    
-    def parse_values(values_str):
-        tokens = values_str.split()
-        values = [float(t) for t in tokens]
-        return values
     
     probe_names = machine_dict['MPNAM2']
-    probe_vals = parse_values(raw['EXPMP2'])
-    probe_weights = parse_selected(raw['FWTMP2'])
+    probe_vals = raw['EXPMP2']
+    probe_weights = raw['FWTMP2']
     probes_dict = OrderedDict()
     for i in range(len(probe_names)):
         probes_dict[probe_names[i]] = [probe_vals[i], probe_weights[i]]
 
     loop_names = machine_dict['LPNAME']
-    loop_vals = parse_values(raw['COILS'])
-    loop_weights = parse_selected(raw['FWTSI'])
+    loop_vals = raw['COILS']
+    loop_weights = raw['FWTSI']
     loops_dict = OrderedDict()
     for i in range(len(loop_names)):
         loops_dict[loop_names[i]] = [loop_vals[i], loop_weights[i]]
         
     if e_coil_names is None:
         e_coil_names = sorted(machine_dict['ECOIL'].keys())
-    e_coil_vals = parse_values(raw['ECURRT'])
-    e_coil_weights = parse_selected(raw['FWTEC'])
+    e_coil_vals = raw['ECURRT']
+    e_coil_weights = raw['FWTEC']
     e_coil_dict = OrderedDict()
     for i in range(len(e_coil_names)):
         e_coil_dict[e_coil_names[i]] = [e_coil_vals[i], e_coil_weights[i]]
     
     if f_coil_names is None:
         f_coil_names = sorted(machine_dict['FCOIL'].keys())
-    f_coil_vals = parse_values(raw['BRSP'])
-    f_coil_weights = parse_selected(raw['FWTFC'])
+    f_coil_vals = raw['BRSP']
+    f_coil_weights = raw['FWTFC']
     f_coil_dict = OrderedDict()
 
     for i in range(len(f_coil_names)):