b4cafe738b54bf7337af2c5d756a330b7b46ad4a
John L <j42558080+1@gmail.com>
2026-04-25 13:22:16 -0400

    Add absolute value to F0 in get_stats

--- src/python/OpenFUSIONToolkit/TokaMaker/_core.py
+++ src/python/OpenFUSIONToolkit/TokaMaker/_core.py
@@ -826,7 +826,7 @@ class TokaMaker():
             'tflux': tflux,
             'l_i': li
         }
-        if self._F0 > 0.0:
+        if abs(self._F0) > 0.0:
             eq_stats['beta_tor'] = 100.0*(2.0*pvol*mu0/vol)/(numpy.power(self._F0/R_geo,2))
             eq_stats['beta_n'] = eq_stats['beta_tor']*eq_stats['a_geo']*(self._F0/R_geo)/(Ip/1.E6)
         return eq_stats
