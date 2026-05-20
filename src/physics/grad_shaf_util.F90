80e8114ef46c9cca2473bd77cb735bea87713741
StuartBenjamin <stubenj9@mit.edu>
2026-05-19 23:15:21 -0400

    Making sure Ip_target is saved/loaded by gs_save_tokamaker, gs_load_tokamaker

--- src/physics/grad_shaf_util.F90
+++ src/physics/grad_shaf_util.F90
@@ -404,7 +404,7 @@ CALL hdf5_write(self%o_point,filename,'tokamaker/O_POINT')
 CALL hdf5_write(self%lim_point,filename,'tokamaker/LIM_POINT')
 CALL hdf5_write(self%diverted,filename,'tokamaker/DIVERTED')
 !---
-IF(self%Itor_target>0.d0)CALL hdf5_write(self%Itor_target,filename,'tokamaker/IP_TARGET')
+IF(self%Itor_target/=-1.d0)CALL hdf5_write(self%Itor_target,filename,'tokamaker/IP_TARGET')
 IF(self%Ip_ratio_target>-1.d98)CALL hdf5_write(self%Ip_ratio_target,filename,'tokamaker/IP_RATIO_TARGET')
 IF(self%R0_target>0.d0)CALL hdf5_write(self%R0_target,filename,'tokamaker/R0_TARGET')
 IF(self%Z0_target>-1.d98)CALL hdf5_write(self%Z0_target,filename,'tokamaker/Z0_TARGET')
@@ -786,6 +786,7 @@ IF(hdf5_field_exist(filename,'tokamaker/IP_TARGET'))THEN
     error_string='Failed to read Ip target.'
     RETURN
   END IF
+  self%Itor_target=ABS(self%Itor_target)
 END IF
 IF(hdf5_field_exist(filename,'tokamaker/IP_RATIO_TARGET'))THEN
   CALL hdf5_read(self%Ip_ratio_target,filename,'tokamaker/IP_RATIO_TARGET',success=success)
