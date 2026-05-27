41b04b94ea93e433f85fef0e99730ce4ef077b0b
Chris Hansen <christopher.hansen@columbia.edu>
2026-05-27 09:14:55 -0400

    Replace single Dockerfile with three Dockerfiles (base, jupyter, extras)  - Add ability to install python dependencies before execution

--- docker/jupyter_entrypoint.sh
+++ docker/jupyter_entrypoint.sh
@@ -10,22 +10,29 @@ print_help() {
   -h Print this help message
   -j Run Jupyter lab server on port 8888
   -s Path to the script to run
-  -n Path to the Jupyter notebook to execute"
+  -n Path to the Jupyter notebook to execute
+  -r Path to requirements.txt file to install dependencies before running the script or notebook"
   return 0  # Success
 }
 
 # Parse arguments
 LAUNCH_JUPYTER=false
 RUN_NOTEBOOK=false
-while getopts ":hjs:n:" opt; do
+while getopts ":hjs:n:r:" opt; do
   case $opt in
     h) print_help; exit 0;;
     j) LAUNCH_JUPYTER=true;;
     s) SCRIPT=$OPTARG;;
     n) SCRIPT=$OPTARG; RUN_NOTEBOOK=true;;
+    r) REQUIREMENTS=$OPTARG;;
     \?) echo "Invalid option"; echo ""; print_help; exit 1;;
   esac
 done
+
+# Install dependencies if requirements.txt provided
+if [[ -n "$REQUIREMENTS" ]]; then
+  pip install --no-cache-dir -r $REQUIREMENTS
+fi
 
 # Execute desired function
 if [ "$LAUNCH_JUPYTER" = true ]; then
