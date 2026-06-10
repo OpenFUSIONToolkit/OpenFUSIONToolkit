968ed1591e54c34ec2d8cfc136b97a0ca0b0d12e
Chris Hansen <christopher.hansen@columbia.edu>
2026-06-09 21:32:50 -0400

    Update release/git information in CD builds

--- src/include/create_git_header.sh
+++ src/include/create_git_header.sh
@@ -4,7 +4,12 @@ if [[ "${HAVE_GIT}" -eq 0 ]]; then
   GITBRANCH=$(git branch | grep "*" | sed "s/* //" | sed "s/ /_/g" | sed -e "s/[()]//g" | sed 's/\r//g')
   GITVERSION=$(git rev-parse --short HEAD | sed 's/\r//g')
   GITTAG=$(git describe --tags --abbrev=0)
-  IS_RELEASE="$(git tag --points-at HEAD | head -n 1)"
+  IS_TAGGED="$(git tag --points-at HEAD | head -n 1)"
+  if [[ -n "$IS_TAGGED" ]]; then
+    IS_RELEASE=true
+  else
+    IS_RELEASE=false
+  fi
 else
   if [[ -z "$GITBRANCH" ]]; then
     GITBRANCH=unknown
@@ -12,9 +17,15 @@ else
   if [[ -z "$GITVERSION" ]]; then
     GITVERSION=unknown
   fi
+  if [[ -z "$GITTAG" ]]; then
+    GITTAG=unknown
+  fi
+  if [[ -z "$IS_RELEASE" ]]; then
+    IS_RELEASE=false
+  fi
 fi
 echo '#define GITTAG "'$GITTAG'"'
-if [[ -z "$IS_RELEASE" ]]; then
+if [[ "$IS_RELEASE" != true ]]; then
   echo '#define GITBRANCH "'$GITBRANCH'"'
   echo '#define GITVERSION "'$GITVERSION'"'
 fi
