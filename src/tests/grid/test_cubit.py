4cd99eb87d367644c595458f83856c5a91e3adfe
Chris Hansen <christopher.hansen@columbia.edu>
2026-09-16 14:21:25 -0400

    Use pytest.approx in Cubit test driver

--- src/tests/grid/test_cubit.py
+++ src/tests/grid/test_cubit.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 test_dir = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.abspath(os.path.join(test_dir, '..')))
-from oft_testing import run_OFT, run_command
+from oft_testing import run_OFT, run_command, approx_list
 
 # Basic template for input file
 oft_in_template = """
@@ -64,31 +64,31 @@ def cubit_setup(nbase, nlevels, prefix, grid_order=1,
     return run_OFT("./test_cubit", nproc, 60)
 
 # Validate results against expected values
-def check_result(volume_test, area_test, nbp_test=None, nbe_test=None, tol=1.E-4):
+def check_result(volume_test, area_test, nbp_test=None, nbe_test=None):
     retval = True
     fid = open('cubit.results','r')
     volume = float(fid.readline())
-    if abs(volume-volume_test) > tol:
+    if volume != volume_test:
         print("FAILED: Incorrect domain volume!")
         print("  Expected = {0}".format(volume_test))
         print("  Actual =   {0}".format(volume))
         retval = False
     area = float(fid.readline())
-    if abs(area-area_test) > tol:
+    if area != area_test:
         print("FAILED: Incorrect domain surface area!")
         print("  Expected = {0}".format(area_test))
         print("  Actual =   {0}".format(area))
         retval = False
     if nbp_test is not None:
         nbp = int(fid.readline())
-        if abs(nbp-nbp_test) > tol:
+        if nbp != nbp_test:
             print("FAILED: Incorrect number of boundary points!")
             print("  Expected = {0}".format(nbp_test))
             print("  Actual =   {0}".format(nbp))
             retval = False
     if nbe_test is not None:
         nbe = int(fid.readline())
-        if abs(nbe-nbe_test) > tol:
+        if nbe != nbe_test:
             print("FAILED: Incorrect number of boundary edges!")
             print("  Expected = {0}".format(nbe_test))
             print("  Actual =   {0}".format(nbe))
@@ -99,14 +99,14 @@ def check_result(volume_test, area_test, nbp_test=None, nbe_test=None, tol=1.E-4
 # Test runners for basic Cubit mesh
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_base(top_lev):
-    volume_cubit = 3.9246
-    area_cubit = 12.1320
+    volume_cubit = pytest.approx(3.9246, abs=1.E-4)
+    area_cubit = pytest.approx(12.1320, abs=1.E-4)
     assert cubit_setup(1,top_lev,'sphere_tet4_test')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_hex_base(top_lev):
-    volume_cubit = 3.91742
-    area_cubit = 12.15673
+    volume_cubit = pytest.approx(3.91742, abs=1.E-4)
+    area_cubit = pytest.approx(12.15673, abs=1.E-4)
     assert cubit_setup(1,top_lev,'sphere_hex8_test')
     assert check_result(volume_cubit, area_cubit)
 
@@ -114,31 +114,31 @@ def test_hex_base(top_lev):
 # Test runners for high order input meshes
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_tet10_quad(top_lev):
-    volume_cubit = 4.18669
-    area_cubit = 12.56224
+    volume_cubit = pytest.approx(4.18669, abs=1.E-4)
+    area_cubit = pytest.approx(12.56224, abs=1.E-4)
     assert cubit_setup(1,top_lev,'sphere_tet10_test',grid_order=2)
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("top_lev", (2, 3))
 @pytest.mark.parametrize("grid_order", (1, 2))
 def test_tet10_1ref(top_lev,grid_order):
-    volume_cubit = [4.12061, 4.18669]
-    area_cubit = [12.4552, 12.5622]
+    volume_cubit = approx_list([4.12061, 4.18669], abs=1.E-4)
+    area_cubit = approx_list([12.4552, 12.5622], abs=1.E-4)
     minlev = 4 - top_lev
     assert cubit_setup(minlev,top_lev,'sphere_tet10_test',grid_order=grid_order)
     assert check_result(volume_cubit[grid_order-1], area_cubit[grid_order-1])
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_hex27_quad(top_lev):
-    volume_cubit = 4.18831
-    area_cubit = 12.56542
+    volume_cubit = pytest.approx(4.18831, abs=1.E-4)
+    area_cubit = pytest.approx(12.56542, abs=1.E-4)
     assert cubit_setup(1,top_lev,'sphere_hex27_test',grid_order=2)
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("top_lev", (2, 3))
 @pytest.mark.parametrize("grid_order", (1, 2))
 def test_hex27_1ref(top_lev,grid_order):
-    volume_cubit = [4.11911, 4.18831]
-    area_cubit = [12.4621, 12.5654]
+    volume_cubit = approx_list([4.11911, 4.18831], abs=1.E-4)
+    area_cubit = approx_list([12.4621, 12.5654], abs=1.E-4)
     minlev = 4 - top_lev
     assert cubit_setup(minlev,top_lev,'sphere_hex27_test',grid_order=grid_order)
     assert check_result(volume_cubit[grid_order-1], area_cubit[grid_order-1])
@@ -147,8 +147,8 @@ def test_hex27_1ref(top_lev,grid_order):
 # Test runners for cut meshes
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_cut_base(top_lev):
-    volume_cubit = 3.918291
-    area_cubit = 12.12685
+    volume_cubit = pytest.approx(3.918291, abs=1.E-4)
+    area_cubit = pytest.approx(12.12685, abs=1.E-4)
     assert cubit_setup(1,top_lev,'sphere_cut_test')
     assert check_result(volume_cubit, area_cubit)
 
@@ -156,26 +156,26 @@ def test_cut_base(top_lev):
 # Test runners for reflection
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_reflect_tri_base(top_lev):
-    volume_cubit = 0.0
-    area_cubit =  2.0
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(2.0, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tri3_test',reflect='T',test_2d='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_reflect_quad_base(top_lev):
-    volume_cubit = 0.0
-    area_cubit =  2.0
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(2.0, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_quad4_test',reflect='T',test_2d='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_reflect_base(top_lev):
-    volume_cubit = 3.02044
-    area_cubit =  12.26295
+    volume_cubit = pytest.approx(3.02044, abs=1.E-4)
+    area_cubit = pytest.approx(12.26295, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tet4_test',reflect='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_reflect_hex_base(top_lev):
-    volume_cubit = 3.00000
-    area_cubit =  12.21166
+    volume_cubit = pytest.approx(3.00000, abs=1.E-4)
+    area_cubit = pytest.approx(12.21166, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_hex8_test',reflect='T')
     assert check_result(volume_cubit, area_cubit)
 
@@ -183,26 +183,26 @@ def test_reflect_hex_base(top_lev):
 # Test runners for periodic reflection
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_perreflect_tri_base(top_lev):
-    volume_cubit = 0.0
-    area_cubit =   2.0
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(2.0, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tri3_test',reflect='T',per_ns=1,test_2d='T')
     assert check_result(volume_cubit, area_cubit, 80, 80)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_perreflect_quad_base(top_lev):
-    volume_cubit = 0.0
-    area_cubit =   2.0
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(2.0, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_quad4_test',reflect='T',per_ns=1,test_2d='T')
     assert check_result(volume_cubit, area_cubit, 80, 80)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_perreflect_tet_base(top_lev):
-    volume_cubit = 3.02044
-    area_cubit =   6.22207
+    volume_cubit = pytest.approx(3.02044, abs=1.E-4)
+    area_cubit = pytest.approx(6.22207, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tet4_test',reflect='T',per_ns=1)
     assert check_result(volume_cubit, area_cubit, 26, 78)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_perreflect_hex_base(top_lev):
-    volume_cubit = 3.00000
-    area_cubit =   6.21166
+    volume_cubit = pytest.approx(3.00000, abs=1.E-4)
+    area_cubit = pytest.approx(6.21166, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_hex8_test',reflect='T',per_ns=1)
     assert check_result(volume_cubit, area_cubit, 24, 48)
 
@@ -210,29 +210,29 @@ def test_perreflect_hex_base(top_lev):
 # Test runners for high order reflected meshes
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_tet10reflect_quad(top_lev):
-    volume_cubit = 3.14123
-    area_cubit =  12.56531
+    volume_cubit = pytest.approx(3.14123, abs=1.E-4)
+    area_cubit =  pytest.approx(12.56531, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tet10_test',grid_order=2,reflect='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("top_lev", (2, 3))
 def test_tet10reflect_1ref(top_lev):
-    volume_cubit = 3.11110
-    area_cubit =  12.48994
+    volume_cubit = pytest.approx(3.11110, abs=1.E-4)
+    area_cubit =  pytest.approx(12.48994, abs=1.E-4)
     minlev = 4 - top_lev
     assert cubit_setup(minlev,top_lev,'ref_tet10_test',reflect='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_hex27reflect_quad(top_lev):
-    volume_cubit = 3.14110
-    area_cubit =  12.56491
+    volume_cubit = pytest.approx(3.14110, abs=1.E-4)
+    area_cubit =  pytest.approx(12.56491, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_hex27_test',grid_order=2,reflect='T')
     assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("top_lev", (2, 3))
 def test_hex27reflect_1ref(top_lev):
-    volume_cubit = 3.10583
-    area_cubit =  12.47691
+    volume_cubit = pytest.approx(3.10583, abs=1.E-4)
+    area_cubit =  pytest.approx(12.47691, abs=1.E-4)
     minlev = 4 - top_lev
     assert cubit_setup(minlev,top_lev,'ref_hex27_test',reflect='T')
     assert check_result(volume_cubit, area_cubit)
@@ -241,8 +241,8 @@ def test_hex27reflect_1ref(top_lev):
 # Test runners for stretched meshes
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_stretch_base(top_lev):
-    volume_cubit = 3.02044
-    area_cubit =  12.26295
+    volume_cubit = pytest.approx(3.02044, abs=1.E-4)
+    area_cubit =  pytest.approx(12.26295, abs=1.E-4)
     assert cubit_setup(1,top_lev,'ref_tet4_test',zstretch=2.)
     assert check_result(volume_cubit, area_cubit)
 
@@ -251,46 +251,46 @@ def test_stretch_base(top_lev):
 @pytest.mark.parametrize("mesh_type", ("tri3", "tri6", "quad4", "quad9"))
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_surf_circle_base(mesh_type,top_lev):
-    volume_cubit = 0.0
-    area_cubit = 3.1403
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(3.1403, abs=1.E-3)
     assert cubit_setup(1,top_lev,'circle_{0}_test'.format(mesh_type),test_2d='T')
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("mesh_type", ("tri3", "tri6", "quad4", "quad9"))
 @pytest.mark.parametrize("levels", ((2, 2), (2, 3), (1, 3)))
 def test_surf_circle_1ref(mesh_type,levels):
-    volume_cubit = 0.0
-    area_cubit = 3.1403
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(3.1403, abs=1.E-3)
     assert cubit_setup(levels[0],levels[1],'circle_{0}_test'.format(mesh_type),test_2d='T')
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("mesh_type", ("tri6", "quad9"))
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_surf_circle_quad(mesh_type,top_lev):
-    volume_cubit = 0.0
-    area_cubit = 3.14160
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(3.14160, abs=1.E-3)
     assert cubit_setup(1,top_lev,'circle_{0}_test'.format(mesh_type),test_2d='T',grid_order=2)
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
 @pytest.mark.parametrize("mesh_type", ("tri3", "quad4"))
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_surf_sphere_base(mesh_type,top_lev):
-    volume_cubit = 0.0
-    area_cubit = 12.5595
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(12.5595, abs=1.E-3)
     assert cubit_setup(1,top_lev,'sphere_{0}_test'.format(mesh_type),test_2d='T')
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("mesh_type", ("tri3", "quad4"))
 @pytest.mark.parametrize("levels", ((2, 2), (2, 3), (1, 3)))
 def test_surf_sphere_1ref(mesh_type,levels):
-    volume_cubit = 0.0
-    area_cubit = 12.5595
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(12.5595, abs=1.E-3)
     assert cubit_setup(levels[0],levels[1],'sphere_{0}_test'.format(mesh_type),test_2d='T')
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
 @pytest.mark.coverage
 @pytest.mark.parametrize("mesh_type", ("tri6", "quad9"))
 @pytest.mark.parametrize("top_lev", (1, 2))
 def test_surf_sphere_quad(mesh_type,top_lev):
-    volume_cubit = 0.0
-    area_cubit = 12.5664
+    volume_cubit = pytest.approx(0.0, abs=1.E-4)
+    area_cubit = pytest.approx(12.5664, abs=1.E-3)
     assert cubit_setup(1,top_lev,'sphere_{0}_test'.format(mesh_type),test_2d='T',grid_order=2)
-    assert check_result(volume_cubit, area_cubit, tol=1.E-3)
+    assert check_result(volume_cubit, area_cubit)
